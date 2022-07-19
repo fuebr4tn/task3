16e16f7e997e489f04b83e7aa38d58012cef00a6
fuebr4tn <artur.mannapov@icloud.com>
2022-07-19 12:29:41 +0300

    Сегодня я написал ёлку!

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int q = in.nextInt();
+
+        for (int i = 0; i<q; i ++) {
+            for (int j = -1; j<i; j ++) {
+                System.out.print("*");
+            }
+            {
+                System.out.println(" ");
+            }
+        }
+    }
+}
